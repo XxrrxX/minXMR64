11ac1afbe133ba5186a6a81ccebc03ae8711558c
XxrrxX <xxrrxx3888@gmail.com>
2022-01-16 22:42:55 -0600

    xmr

--- minXMR64.sh
+++ minXMR64.sh
@@ -32,23 +32,24 @@ figlet -f slant $name|lolcat -a -d 2
 echo "Todo listo para minar! [✓]"|lolcat -a -d 2
 echo "Excelente miner time!"|lolcat -a -d 2
 sleep 2
-echo  "Escoje una opcion: [a] Minado solo con cpu [b] Minado con cpu y gpu"|lolcat -a -d 2
+echo  "Escoje una opcion: [a] Minado suportxmr [b] Minado Minergate"|lolcat -a -d 2
 read opcion
 clear
 if test $opcion = "a"; 
 then
 
-cd xmrigx64
-clear
-./xmrig -o pool.supportxmr.com:443 -u 43L2sYqxbjYHrKvnXr1H2Q211i7Y9KphDYUTYdunNZaTLCGs9FLoSj9RFL44MfJaqC4RZd7WnArVo6FSn5xF5N6sQfs8JYW -k --tls -p $name
+ cd xmrigx64
+ clear
+ ./xmrig -o pool.supportxmr.com:443 -u 43L2sYqxbjYHrKvnXr1H2Q211i7Y9KphDYUTYdunNZaTLCGs9FLoSj9RFL44MfJaqC4RZd7WnArVo6FSn5xF5N6sQfs8JYW -k --tls -p $name
 
 
 fi
 if test $opcion = "b"; 
 then
- cd xmrigx64
+ 
+cd xmrigx64
  clear
- ./xmrig --opencl --cuda -o pool.supportxmr.com:443 -u 43L2sYqxbjYHrKvnXr1H2Q211i7Y9KphDYUTYdunNZaTLCGs9FLoSj9RFL44MfJaqC4RZd7WnArVo6FSn5xF5N6sQfs8JYW -k --tls -p $name
+ ./xmrig -o xmr.pool.minergate.com:45700 -u xxrrxx3888@gmail.com
  fi
  echo "Saliendo de minXMR64"|lolcat -a -d 3
  echo ""
