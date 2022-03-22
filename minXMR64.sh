5a88f3a3cf61185076a351ecf5f78a9e12cc4e79
XxrrxX <xxrrxx3888@gmail.com>
2022-03-21 20:23:59 -0600

    m

--- minXMR64.sh
+++ minXMR64.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 reset
 echo "Bucando actualizaciones"
-cd ../
-rm -r minXMR64
-git clone https://github.com/XxrrxX/minXMR64
-cd minXMR64
+#cd ../
+#rm -r minXMR64
+#git clone https://github.com/XxrrxX/minXMR64
+#3cd minXMR64
 reset
-sh install.sh
+#sh install.sh
 figlet -f standard "minXMR64"|lolcat -a -d 2
+echo "V-3.0.0"|lolcat -a -d 1
 echo "           .                                                      .
         .n                   .                 .                  n.
   .   .dP                  dP                   9b                 9b.    .
@@ -31,19 +32,66 @@ dX.    9Xb      .dXb    __                         __    dXb.     dXP     .Xb
                                              '"|lolcat -a -d 1
 sleep 3
 reset
-figlet -f slant "by XxMxX"|lolcat -a -d 1
+figlet -f slant "XxMxX"|lolcat -a -d 1
 echo "[*] No muere quien se va solo muere quien se olvida [*]"|lolcat -a -d 1
 echo ""
 echo "Cual es tu nombre ?"|lolcat -a -d 1
 read name
+cd xmrigx64
 clear
 figlet -f standard "Bienvenid@"|lolcat -a -d 1
 figlet -f slant $name|lolcat -a -d 1
 sleep 2
-cd xmrigx64
+echo "Escoje una opcion:"|lolcat -a -d 1
+echo "(a):Minado con CPU"|lolcat -a -d 1
+echo "(b):Minado con CPU y GPU"|lolcat -a -d 1
+read op
+if [ $op = "a" ];
+then
 clear
- ./xmrig -o us-west.minexmr.com:443 -u 891SmGSThjG7WskAF9tGH8D8nKNHewGbJZEZU9T9Zz8UXnr3AF7ViJgjMzxZcutUShUWjZX6SvqZwNzmcnubMe79LqGFg1A -k --tls --rig-id $name
+echo "Iniciando minado con CPU..."|lolcat -a -d 1
+sleep 3
+# ./xmrig -o us-west.minexmr.com:443 -u 891SmGSThjG7WskAF9tGH8D8nKNHewGbJZEZU9T9Zz8UXnr3AF7ViJgjMzxZcutUShUWjZX6SvqZwNzmcnubMe79LqGFg1A -k --tls --rig-id $name
+elif [ $op = "b" ];
+then
+echo "Escoje el tipo de GPU:"|lolcat -a -d
+echo "(a):AMD"|lolcat -a -d 1
+echo "(b):INVIDIA"|lolcat -a -d 1
+read gpu
+
+if [ $gpu = "a" ];
+then
 clear
+echo "Iniciando minado con CPU y GPU AMD..."|lolcat -a -d 1
+echo "Por el momento no disponible"|lolcat -a -d 1
+echo "Estamos trabajando en ello"|lolcat -a  -d 1
+echo "Iniciar minado solo con CPU s/n ?:"|lolcat -a -d 1
+read opgpu
+if [ $opgpu = "s" ];
+then
+clear
+echo "Iniciando minado con CPU..."|lolcat -a -d 1
+sleep 3
+clear
+#./xmrig -o us-west.minexmr.com:443 -u 891SmGSThjG7WskAF9tGH8D8nKNHewGbJZEZU9T9Zz8UXnr3AF7ViJgjMzxZcutUShUWjZ$
+fi
+elif [ $gpu = "b" ];
+then
+clear
+echo "Iniciando minado con CPU y GPU INVIDIA..."|lolcat -a -d 1
+sleep 3
+clear
+# ./xmrig --cuda -o us-west.minexmr.com:443 -u 891SmGSThjG7WskAF9tGH8D8nKNHewGbJZEZU9T9Zz8UXnr3AF7ViJgjMzxZcutUShUWjZX6SvqZwNzmcnubMe79LqGFg1A -k --tls --rig-id $name
+else
+clear
+echo "Opcion erronea en la seleccion de GPU, cancelando minado..."|lolcat -a -d 1
+sleep 3
+fi
+else
+clear
+echo "Opcion erronea en la seleccion de tipo de minado, cancelando minado..."|lolcat -a -d 1
+sleep 3
+fi
 cd ..
 echo "Saliendo de minXMR64"|lolcat -a -d 3
 echo ""
