3081d45328fbaa56e19e3e455ef2654f3c3c00d5
XxrrxX <xxrrxx3888@gmail.com>
2022-08-15 23:53:20 -0500

    se actualiza

--- minXD.sh
+++ minXD.sh
@@ -3,33 +3,33 @@ reset
 echo "Bucando actualizaciones"
 cd ../
 rm -r minXMR64
-git clone https://github.com/XxrrxX/minXMR64
+git clo
+ne https://github.com/XxrrxX/minXMR64
 cd minXMR64
 reset
 sh install.sh
 figlet -f standard "minXMR64"|lolcat -a -d 2
-echo "V-6.0.0"|lolcat -a -d 1
-echo "           .                                                      .
-        .n                   .                 .                  n.
-  .   .dP                  dP                   9b                 9b.    .
- 4    qXb         .       dX                     Xb       .        dXp     t
-dX.    9Xb      .dXb    __                         __    dXb.     dXP     .Xb
-9XXb._       _.dXXXXb dXXXXbo.                 .odXXXXb dXXXXb._       _.dXXP
- 9XXXXXXXXXXXXXXXXXXXVXXXXXXXXOo.           .oOXXXXXXXXVXXXXXXXXXXXXXXXXXXXP
-   9XXXXXXXXXXXXXXXXXXXXX~   ~ OOO8b   d8OOO'~  ~ XXXXXXXXXXXXXXXXXXXXXP
-     9XXXXXXXXXXXP   9XX    DIE    98v8P   HUMAN    XXP   9XXXXXXXXXXXP
-        ~~~~~~~       9X.          .db db.          .XP       ~~~~~~~
-                         b.  .dbo.dP' v 9b.odb.  .dX
-                      ,dXXXXXXXXXXXb     dXXXXXXXXXXXb.
-                     dXXXXXXXXXXXP'   .    9XXXXXXXXXXXb
-                    dXXXXXXXXXXXXb   d b   dXXXXXXXXXXXXb
-                    9XXb'    XXXXXb.dX Xb.dXXXXX     dXXP
-                             9XXXXXX     XXXXXXP
-                              XXXX X. v .X XXXX
-                              XP^X  b   d'p^XX
-                              X. 9      '  P  X
-                               b             d
-                                             '"|lolcat -a -d 1
+echo "V-7.0.0"|lolcat -a -d 1
+echo " 　　███
+　　　　　██▒▒██
+　　　　█▒▒▒▒▒██
+　　　██▒▒▒▓▓▒▒██　　█
+　　　█▒▒▒▓▓▓▒▒▒█　　　█
+　　██▒▒▒▓▓▓▓▒▒█　　　█
+　　█▒▒▒▓▓▓▓▓▒▒█　████████
+　　██▒▒▓▓▓▓▓▒▒█　█████　　█
+　　　█▒▒▒▓▓▓▒▒▒██████
+　　　██▒▒▒▓▒▒▒█████████████████
+　　████▒▒▒▒▒██████▒▒▒▒▒▒▒▒▒▒▒▒███
+　██▒▒██▒▒██████▒▒▒▓▓▓▓▓▓▒▒▒▒▒██
+██▒▒▒▒▒▒█████▒▒▒▒▓▓▓▓▓▓▓▓▒▒▒███
+　██▒▒▒▒███▒██▒▒▒▒▓▓▓▓▓▒▒▒▒██
+　　██████▒▒▒▒██▒▒▒▒▒▒▒▒▒████
+　　　　　█▒▒▒▒▒▒▒██████████
+　　　　　██▒▒▒▒▒▒██
+　　　　　　　█▒▒▒▒██
+　　　　　　　██████
+"|lolcat -a -d 1
 sleep 3
 reset
 figlet -f slant "XxMxX"|lolcat -a -d 1
@@ -85,26 +85,26 @@ else
 if [ $coin = "a" ]; #xmr
 then
 echo "Escoje una opcion:"|lolcat -a -d 1
-echo "(a):Minado con randomx"|lolcat -a -d 1
-echo "(b):Minado con ethash"|lolcat -a -d 1
+echo "(a):Minado XMR 3GB"|lolcat -a -d 1
+echo "(b):Minado XMR ethash"|lolcat -a -d 1
 elif [ $coin = "b" ]; #bnb
 then
 echo "Escoje una opcion:"|lolcat -a -d 1
-echo "(a):Minado con CPU y GPU algoritmo randomx"|lolcat -a -d 1
-echo "(b):Minado con GPU algoritmo ethash"|lolcat -a -d 1
+echo "(a):Minado BNB 3GB"|lolcat -a -d 1
+echo "(b):Minado BNB ethash"|lolcat -a -d 1
 fi
 read op
 
-if [ $op = "a" ]; #randomx
+if [ $op = "a" ]; #3GB
 then
 if [ $coin = "a" ]; #conf xmr
 then
-pool="rx.unmineable.com"
-configuracion="--cuda -o "$pool":"$puerto" -a rx -k -u XMR:"$addres.$name" -p X"
+pool="xmr.unmineable.com"
+configuracion="-a ethash -o "stratum+tcp://$pool":"$puerto" -a rx -k -u XMR:"$addres.$name""
 elif [ $coin = "b" ]; #conf bnb
 then
-pool="rx.unmineable.com"
-configuracion="--cuda -o "$pool":"$puerto" -a rx -k -u BNB:"$addres.$name" -p x "
+pool="ethash.unmineable.com"
+configuracion="-a ethash -o "stratum+tcp://$pool":"$puerto" -u BNB:"$addres.$name""
 fi
 
 clear
