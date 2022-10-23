b34dd61eb878d31074e2601493a5e09f44bb5380
XxrrxX <79820165+XxrrxX@users.noreply.github.com>
2022-10-23 17:59:08 -0500

    Update minXD.sh

--- minXD.sh
+++ minXD.sh
@@ -111,8 +111,8 @@ if [ $op = "a" ]; #Autolykos
 then
 if [ $coin = "a" ]; #conf xrp
 then
-pool="stratum+ssl://autolykos.unmineable.com"
-configuracion="-a autolykos2 -o "$pool":"$puerto" -u XRP:"$addres.$memo.$name" -p x --no-strict-ssl"
+pool="autolykos.unmineable.com"
+configuracion="-a autolykos2 -o "$pool":"$puerto" -u XRP:"$addres.$memo.$name" -p x"
 elif [ $coin = "b" ]; #conf bnb
 then
 pool="autolykos.unmineable.com"
