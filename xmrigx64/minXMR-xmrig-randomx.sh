01d1a7c535456edc4948cf6f38f6df8e439017d7
XxrrxX <xxrrxx3888@gmail.com>
2022-11-08 01:33:56 -0600

    se actualiza minXMR64

--- xmrigx64/minXMR-xmrig-randomx.sh
+++ xmrigx64/minXMR-xmrig-randomx.sh
@@ -1,1 +1,13 @@
-./xmrig --cuda -a rx/0 -o ca.monero.herominers.com:10191 -u 891SmGSThjG7WskAF9tGH8D8nKNHewGbJZEZU9T9Zz8UXnr3AF7ViJgjMzxZcutUShUWjZX6SvqZwNzmcnubMe79LqGFg1A -k --tls -p XxrrxX
+echo "Escoje el tipo de minado :"|lolcat -a -d 1
+echo "(a) minado con CPU y GPU"|lolcat -a -d 1
+echo "(b) minado con GPU"|lolcat -a -d 1
+read op
+echo "Cual es tu nombre?:"|lolcat -a -d 1
+read nombre
+if [ $op = a ];
+then
+./xmrig --cuda -a rx/0 -o ca.monero.herominers.com:10191 -u 891SmGSThjG7WskAF9tGH8D8nKNHewGbJZEZU9T9Zz8UXnr3AF7ViJgjMzxZcutUShUWjZX6SvqZwNzmcnubMe79LqGFg1A -k --tls -p $nombre
+elif [ $op = "b" ];
+then
+./xmrig --no-cpu --cuda -a rx/0 -o ca.monero.herominers.com:10191 -u 891SmGSThjG7WskAF9tGH8D8nKNHewGbJZEZU9T9Zz8UXnr3AF7ViJgjMzxZcutUShUWjZX6SvqZwNzmcnubMe79LqGFg1A -k --tls -p $nombre
+fi
