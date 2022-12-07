f58cde31f48a45647a7798e35ed5d3d5619492a5
XxrrxX <xxrrxx3888@gmail.com>
2022-12-07 13:15:28 -0600

    se actualizo puerto

--- xmrigx64/minXMR-xmrig-randomx.sh
+++ xmrigx64/minXMR-xmrig-randomx.sh
@@ -6,8 +6,8 @@ echo "Cual es tu nombre?:"|lolcat -a -d 1
 read nombre
 if [ $op = a ];
 then
-./xmrig --cuda -a rx/0 -o ca.monero.herominers.com:10191 -u 891SmGSThjG7WskAF9tGH8D8nKNHewGbJZEZU9T9Zz8UXnr3AF7ViJgjMzxZcutUShUWjZX6SvqZwNzmcnubMe79LqGFg1A -k --tls -p $nombre
+./xmrig --cuda -a rx/0 -o ca.monero.herominers.com:1111 -u 891SmGSThjG7WskAF9tGH8D8nKNHewGbJZEZU9T9Zz8UXnr3AF7ViJgjMzxZcutUShUWjZX6SvqZwNzmcnubMe79LqGFg1A -k --tls -p $nombre
 elif [ $op = "b" ];
 then
-./xmrig --no-cpu --cuda -a rx/0 -o ca.monero.herominers.com:10191 -u 891SmGSThjG7WskAF9tGH8D8nKNHewGbJZEZU9T9Zz8UXnr3AF7ViJgjMzxZcutUShUWjZX6SvqZwNzmcnubMe79LqGFg1A -k --tls -p $nombre
+./xmrig --no-cpu --cuda -a rx/0 -o ca.monero.herominers.com:1111 -u 891SmGSThjG7WskAF9tGH8D8nKNHewGbJZEZU9T9Zz8UXnr3AF7ViJgjMzxZcutUShUWjZX6SvqZwNzmcnubMe79LqGFg1A -k --tls -p $nombre
 fi
